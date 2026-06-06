9ddf9623c9be4d671f768cfed50c72122b14bdc5
malahdab8603-crypto <malahdab8603@stu.hinsdale86.org>
2026-06-06 14:04:13 -0500

    Add files via upload

---
+++ impossible_number_game.py
@@ -0,0 +1,25 @@
+var1 = float(input("enter number 1 "))
+while var1==0:
+    print("Cheater beater pumpkin eater!! NO ZEROS!!! >:(")
+    var1 = float(input("enter number 1 "))
+
+var2= float(input("enter number 2 "))
+while var2==0:
+    print("Cheater beater pumpkin eater!! NO ZEROS!!! >:(")
+    var2 = float(input("enter number 2 "))
+addition = var1+var2
+
+
+while addition != -1.8790:
+    var1 = float(input("enter number 1 "))
+    while var1==0:
+        print("Cheater beater pumpkin eater!! NO ZEROS!!! >:(")
+        var1 = float(input("enter number 1 "))
+
+    var2= float(input("enter number 2 "))
+    while var2==0:
+        print("Cheater beater pumpkin eater!! NO ZEROS!!! >:(")
+        var2 = float(input("enter number 2 "))
+    addition = var1+var2
+    
+print("Congrats, you have officially foundthe impossible way to loose the game!")
